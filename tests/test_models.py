36ce2cefb7717e70d34fee028de00cde52c37744
Theo-Par <tp6g24@soton.ac.uk>
2026-01-28 15:10:24 +0000

    Add test cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,7 +4,7 @@ import pytest
 import numpy as np
 import numpy.testing as npt
 
-from inflammation.models import daily_max, daily_mean, daily_min
+from inflammation.models import daily_max, daily_mean, daily_min, patient_normalise
 
 
 @pytest.mark.parametrize(
@@ -40,6 +40,69 @@ def test_daily_max(test, expected):
 def test_daily_min(test, expected):
     """Test min function works for zeroes, positive integers, mix of positive/negative integers."""
     npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
+
+
+@pytest.mark.parametrize(
+    "test, expected, expect_raises",
+    [
+        (
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+            None,
+        ),
+        (
+            [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+            [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+            None,
+        ),
+        (
+            [[float('nan'), 1, 1], [1, 1, 1], [1, 1, 1]],
+            [[0, 1, 1], [1, 1, 1], [1, 1, 1]],
+            None,
+        ),
+        (
+            [[1, 2, 3], [4, 5, float('nan')], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.8, 1, 0], [0.78, 0.89, 1]],
+            None,
+        ),
+        (
+            [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            ValueError,
+        ),
+        (
+            'hello',
+            None,
+            TypeError,
+        ),
+        (
+            3,
+            None,
+            TypeError,
+        ),
+        (
+            [4, 5, 6],
+            None,
+            ValueError,
+        ),
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None,
+        ),
+    ])
+def test_patient_normalise(test, expected, expect_raises):
+    """Test normalisation works for arrays of one and positive integers.
+       Test with a relative and absolute tolerance of 0.01."""
+
+    if isinstance(test, list):
+        test = np.array(test)
+    if expect_raises is not None:
+        with pytest.raises(expect_raises):
+          patient_normalise(test)
+    else:
+        result = patient_normalise(test)
+        npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
 
 
 """
